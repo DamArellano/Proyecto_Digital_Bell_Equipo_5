f292036be82afa9aef38bc4fa09aadb61162a65c
LuMaBaMa <lbarriosm2100@alumno.ipn.mx>
2024-12-09 18:30:52 -0600

    Mas cerca

--- Prototipo Personal Barrios/Principal.py
+++ Prototipo Personal Barrios/Principal.py
@@ -16,7 +16,7 @@ def main():
         "Registro_Habitante": Logica_Habitante.Registrar_Habitante,
         "Enviar_Queja": Logica_Habitante.QuejaHa,
         "Ver_Horarios": Logica_Habitante.Mostrar_Horarios,
-        "Mostrar_Horarios": Logica_Habitante.Mostrar_Horarios,
+        "Mostrar_Horarios": Logica_Empleado.Mostrar_Horarios,
         "Registro_Empleado": Logica_Empleado.Registrar_Empleado,
         "Horarios": Logica_Administrador.AsH,
     }
